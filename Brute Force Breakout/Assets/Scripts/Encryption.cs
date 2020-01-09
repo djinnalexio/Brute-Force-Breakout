fe0cacdf05663952d7f462f8c61c85328d1ee59c
pakueamb <pakueamb@gmail.com>
2020-01-09 16:52:45 -0500

    rewrote Keypad encryption
    - arrows are now set active during the keypress if 3 digits have been entered instead of checking each frame
    
    - there is now only one Enter function that can take an 'int' paramater instead of coping/pasting for each digit.

--- Brute Force Breakout/Assets/Scripts/Encryption.cs
+++ Brute Force Breakout/Assets/Scripts/Encryption.cs
@@ -21,33 +21,18 @@ public class Encryption : MonoBehaviour
         checkLight.SetActive(false);
     }
 
-    void Update()
-    {
-        if (realCode.Length == 3)
+    public void Enter(int i)
+    { 
+        if (realCode.Length < 3) 
         {
-            foreach (GameObject i in arrows) { i.SetActive(true); }
+            realCode += i.ToString();
+            displayCode.text = realCode;
+        }
+
+        if (realCode.Length >= 3)
+        {
+            foreach (GameObject j in arrows) { j.SetActive(true); }
             checkLight.SetActive(true);
         }
     }
-
-    public void Enter0()
-    { if (realCode.Length < 3) { realCode += '0'; displayCode.text = realCode; } }
-    public void Enter1()
-    { if (realCode.Length < 3) { realCode += '1'; displayCode.text = realCode; } }
-    public void Enter2()
-    { if (realCode.Length < 3) { realCode += '2'; displayCode.text = realCode; } }
-    public void Enter3()
-    { if (realCode.Length < 3) { realCode += '3'; displayCode.text = realCode; } }
-    public void Enter4()
-    { if (realCode.Length < 3) { realCode += '4'; displayCode.text = realCode; } }
-    public void Enter5()
-    { if (realCode.Length < 3) { realCode += '5'; displayCode.text = realCode; } }
-    public void Enter6()
-    { if (realCode.Length < 3) { realCode += '6'; displayCode.text = realCode; } }
-    public void Enter7()
-    { if (realCode.Length < 3) { realCode += '7'; displayCode.text = realCode; } }
-    public void Enter8()
-    { if (realCode.Length < 3) { realCode += '8'; displayCode.text = realCode; } }
-    public void Enter9()
-    { if (realCode.Length < 3) { realCode += '9'; displayCode.text = realCode; } }
 }
